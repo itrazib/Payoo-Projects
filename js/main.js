8339a943011f1f2630e3fbccc7c80eeb67d27d09
Razib Das <it.razib.mbstu@gmail.com>
2025-08-25 11:13:44 +0600

    added main.js reusable method

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 const validPin = 1234;
+// reusable function
+function getInputValueNumber(id){
+    const value = parseInt(document.getElementById(id).value);
+    return value;
+}
+function getInputValue(id){
+    const value = document.getElementById(id).value;
+    return value;
+}
+
+function getInnerText(id){
+    const value =  parseInt(document.getElementById(id).innerText);
+    return value;
+}
+function setInnerText(value){
+   document.getElementById('amount').innerText = value;
+}
+
 
 document.getElementById('add-money-btn').addEventListener('click', function(event){
     event.preventDefault();
-     const bankName = document.getElementById('add-bank').value;
+     const bankName = getInputValue('add-bank');
 
-     const accountNum = document.getElementById('add-acc-num').value;
-     const addAmount = parseInt(document.getElementById('add-amount').value);
-     const pin = parseInt(document.getElementById('pin-num').value);
-     ;
-    //  console.log(bankName, accountNum, addAmount, pin);
+     const accountNum = getInputValue('add-acc-num')
+
+     const addAmount = getInputValueNumber('add-amount')
+
+     const pin = getInputValueNumber('pin-num');
+    
  if(accountNum.length !== 11){
     alert("Please provide a valid account number");
     return;
@@ -17,14 +36,14 @@ if(pin != validPin){
     alert('Please provide a valid pin number');
     return;
 }
-    const amount = parseInt(document.getElementById('amount').innerText);
+    const amount = getInnerText('amount');
     const totalAmount = amount + addAmount;
-    document.getElementById('amount').innerText = totalAmount;
+    setInnerText(totalAmount);
 })
 document.getElementById('cash-out-btn').addEventListener('click', function(event){
     event.preventDefault();
-    const agentNumber = document.getElementById('agent-number').value;
-    const cashPin = parseInt(document.getElementById('cash-out-pin').value);
+    const agentNumber = getInputValue('agent-number');
+    const cashPin = getInputValueNumber('cash-out-pin');
     if(agentNumber.length !== 11){
         alert('Please provide valid Account number');
         return;
@@ -33,9 +52,9 @@ document.getElementById('cash-out-btn').addEventListener('click', function(event
     alert('Please provide a valid pin number');
     return;
 }
-    const withdrawAmount = parseInt(document.getElementById('cash-out-amount').value);
-     const availableAmount = parseInt(document.getElementById('amount').innerText);
-     document.getElementById('amount').innerText = availableAmount - withdrawAmount
+    const withdrawAmount = getInputValueNumber('cash-out-amount')
+     const availableAmount = getInnerText('amount');
+     setInnerText(availableAmount - withdrawAmount);
 
 })
 
